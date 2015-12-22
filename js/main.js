eb1fe62e80b4788caf29b466289b787c76585c13
Jason Wong <jason_19960903@hotmail.com>
2015-12-21 19:10:59 -0800

    Custom Active for Nav and Changes active when scrolled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){  
   navbarColorChange();
   navitemClicked();
+  navIntroActive();
+  navPortfolioActive();
+  navContactActive();
 });
 
 // navbar color changes when scrolled to introduction div
@@ -27,19 +30,73 @@ var navitemClicked = function(){
       scrollTop: $(".coverpage").offset().top-50},
       'slow');
   });
-  $("#nav-intro").click(function(){
+  $("#nav-intro a").click(function(){
     $("html,body").animate({
       scrollTop: $(".introduction").offset().top-50},
       'slow');
   });
-  $("#nav-portfolio").click(function(){
+  $("#nav-portfolio a").click(function(){
     $("html,body").animate({
       scrollTop: $(".portfolio").offset().top-50},
       'slow');
   });
-  $("#nav-contact").click(function(){
+  $("#nav-contact a").click(function(){
     $("html,body").animate({
-      scrollTop: $(".contact").offset().top-50},
+      scrollTop: $(".contact").offset().top - 50},
       'slow');
   });
+}
+// when scrolled to introduction, nav-intro becomes active
+var navIntroActive = function(){
+  var scroll_start = 0;
+  var startchange = $('.introduction');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top - 50.5) {
+        $(".nav li").removeClass("active");
+        $("#nav-intro").addClass("active");
+        console.log("at Intro");
+      } else {
+        $("#nav-intro").removeClass("active");
+      }
+    });
+  }
+}
+// when scrolled to portfolio, nav-portfolio becomes active
+var navPortfolioActive = function(){
+  var scroll_start = 0;
+  var startchange = $('.portfolio');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top - 50.5) {
+        $(".nav li").removeClass("active");
+        $("#nav-portfolio").addClass("active");
+        console.log("at portfolio");
+      } else {
+        $("#nav-portfolio").removeClass("active");
+      }
+    });
+  }
+}
+// when scrolled to contact, nav-contact becomes active
+var navContactActive = function(){
+  var scroll_start = 0;
+  var startchange = $('.contact');
+  var offset = startchange.offset();
+  if (startchange.length){
+    $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top - 80) {
+        $(".nav li").removeClass("active");
+        $("#nav-contact").addClass("active");
+        console.log("hi");
+      } else {
+        $("#nav-contact").removeClass("active");
+      }
+    });
+  }
 }
