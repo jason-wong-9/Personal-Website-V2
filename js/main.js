5a35f06fff374332fbc3d5f8b17315a1611c7739
Jason Wong <jason_19960903@hotmail.com>
2015-12-21 19:54:06 -0800

    Fixed up the offset number to make it smoother

--- js/main.js
+++ js/main.js
@@ -4,12 +4,13 @@ $(document).ready(function(){
   navIntroActive();
   navPortfolioActive();
   navContactActive();
+  changeActiveOnScroll();
 });
 
 // navbar color changes when scrolled to introduction div
 var navbarColorChange = function(){
   var scroll_start = 0;
-  var startchange = $('.introduction');
+  var startchange = $('#introduction');
   var offset = startchange.offset();
   if (startchange.length){
     $(document).scroll(function() { 
@@ -27,39 +28,39 @@ var navbarColorChange = function(){
 var navitemClicked = function(){
   $("#nav-home").click(function(){
     $("html,body").animate({
-      scrollTop: $(".coverpage").offset().top-50},
+      scrollTop: $("#coverpage").offset().top-50},
       'slow');
   });
-  $("#nav-intro a").click(function(){
+  $("#nav-introduction a").click(function(){
     $("html,body").animate({
-      scrollTop: $(".introduction").offset().top-50},
+      scrollTop: $("#introduction").offset().top-50},
       'slow');
   });
   $("#nav-portfolio a").click(function(){
     $("html,body").animate({
-      scrollTop: $(".portfolio").offset().top-50},
+      scrollTop: $("#portfolio").offset().top-50},
       'slow');
   });
   $("#nav-contact a").click(function(){
     $("html,body").animate({
-      scrollTop: $(".contact").offset().top - 50},
+      scrollTop: $("#contact").offset().top - 50},
       'slow');
   });
 }
 // when scrolled to introduction, nav-intro becomes active
 var navIntroActive = function(){
   var scroll_start = 0;
-  var startchange = $('.introduction');
+  var startchange = $('#introduction');
   var offset = startchange.offset();
   if (startchange.length){
     $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
       if(scroll_start > offset.top - 50.5) {
         $(".nav li").removeClass("active");
-        $("#nav-intro").addClass("active");
+        $("#nav-introduction").addClass("active");
         console.log("at Intro");
       } else {
-        $("#nav-intro").removeClass("active");
+        $("#nav-introduction").removeClass("active");
       }
     });
   }
@@ -67,7 +68,7 @@ var navIntroActive = function(){
 // when scrolled to portfolio, nav-portfolio becomes active
 var navPortfolioActive = function(){
   var scroll_start = 0;
-  var startchange = $('.portfolio');
+  var startchange = $('#portfolio');
   var offset = startchange.offset();
   if (startchange.length){
     $(document).scroll(function() { 
@@ -85,12 +86,12 @@ var navPortfolioActive = function(){
 // when scrolled to contact, nav-contact becomes active
 var navContactActive = function(){
   var scroll_start = 0;
-  var startchange = $('.contact');
+  var startchange = $('#contact');
   var offset = startchange.offset();
   if (startchange.length){
     $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top - 80) {
+      if(scroll_start > offset.top - 500) {
         $(".nav li").removeClass("active");
         $("#nav-contact").addClass("active");
         console.log("hi");
