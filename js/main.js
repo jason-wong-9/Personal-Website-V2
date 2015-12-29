68905ee944b7f62fa174b1bc2068d6e98fc2930d
Jason Wong <jason_19960903@hotmail.com>
2015-12-29 14:15:19 -0800

    Bounce Up

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     // navIntroActive();
     // navPortfolioActive();
     // navContactActive();
+    $(window).on('scroll resize', check_if_in_view);
+    $(window).trigger('scroll');
 });
 
 // navbar color changes when scrolled to introduction div
@@ -59,6 +61,32 @@ var collapse = function() {
         $(".navbar-collapse").collapse('hide');
     });
 }
+
+
+
+function check_if_in_view() {
+    var $animation_elements = $('.animation-element');
+var $window = $(window);
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+      (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
 
 
 // when scrolled to introduction, nav-intro becomes active
