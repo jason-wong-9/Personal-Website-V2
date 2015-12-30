5e4b62f7ccfa223225942f67796b5da1905ee210
Jason Wong <jason_19960903@hotmail.com>
2015-12-29 23:39:12 -0800

    Added images to Carousel

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
     // navContactActive();
     $(window).on('scroll resize', check_if_in_view);
     $(window).trigger('scroll');
+//    autoCarousel();
 });
 
 // navbar color changes when scrolled to introduction div
@@ -66,27 +67,32 @@ var collapse = function() {
 
 function check_if_in_view() {
     var $animation_elements = $('.animation-element');
-var $window = $(window);
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+    var $window = $(window);
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+    $.each($animation_elements, function() {
+        var $element = $(this);
+        var element_height = $element.outerHeight();
+        var element_top_position = $element.offset().top;
+        var element_bottom_position = (element_top_position + element_height);
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
+        //check to see if this current container is within viewport
+        if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+            $element.addClass('in-view');
+        } else {
+            $element.removeClass('in-view');
+        }
+    });
 }
 
+// automatically go to next image in carousel
+function autoCarousel() {
+    $('.carousel').carousel({
+        interval: 3000
+    });
+}
 
 
 // when scrolled to introduction, nav-intro becomes active
