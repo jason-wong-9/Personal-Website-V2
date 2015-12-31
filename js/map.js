061b71adab52ed7a591a0d973de6f16ada118836
Jason Wong <jason_19960903@hotmail.com>
2015-12-31 10:45:36 -0800

    Message Form Setup

---
+++ js/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+    var myLatLng = {lat: 49.246292, lng: -123.116226};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: myLatLng,
+        scrollwheel: false
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Vancouver'
+    });
+}
+        
